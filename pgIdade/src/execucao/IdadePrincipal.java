b1c360a2e123027458c21ac653486053b8242e7d
Diego <Diego@Diego>
2019-06-26 12:41:02 -0300

    Foi

---
+++ pgIdade/src/execucao/IdadePrincipal.java
@@ -0,0 +1,17 @@
+package execucao;
+// Usando if se a idade dele for menor que 18 anos
+import javax.swing.JOptionPane;
+
+public class IdadePrincipal {
+	public static void main(String[] args) {
+		String nome = JOptionPane.showInputDialog("Digite seu nome");
+		int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a sua idade"));
+		if(idade <18){
+			System.out.println("É menor !:" + idade);
+		} else {
+			System.out.println("É maior que 18 anos: " + idade);
+		}
+		
+	}
+
+}

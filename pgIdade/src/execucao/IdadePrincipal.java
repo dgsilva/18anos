275c637474c0faa4c4588298d162e749cd9e3127
dgsilva <diegorj93@gmail.com>
2019-11-28 19:22:29 -0300

    Initial commit

--- pgIdade/src/execucao/IdadePrincipal.java
+++ pgIdade/src/execucao/IdadePrincipal.java
@@ -7,9 +7,9 @@ public class IdadePrincipal {
 		String nome = JOptionPane.showInputDialog("Digite seu nome");
 		int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a sua idade"));
 		if(idade <18){
-			System.out.println("É menor !:" + idade);
+			System.out.println("É menor!:" + idade);
 		} else {
-			System.out.println("É maior que 18 anos: " + idade);
+			System.out.println("É maior que 18 anos:" + idade);
 		}
 		
 	}
